252d7c325f12c203bc6c459515029377bba3de83
justin friedman <justinfriedman22@gmail.com>
2017-09-23 15:19:43 -0500

    made color change in sandbox

--- javascript.js
+++ javascript.js
@@ -1,3 +1,39 @@
+//users
+var sessionUser;
+
+var user1 = {
+        door: {
+                state:0,  // 0 for down, 1 for coming down, 2 for going up, 3 for up
+                temp: 72,
+                hum: 34,
+                motion: true, // boolean
+
+        },
+        email:"rocketman@up.nk",
+        password:"bestkorea",
+        phone:"5555555555",
+        id:1234563
+}
+var user2 = {
+        door: {
+                state:3,  // 0 for down, 1 for coming down, 2 for going up, 3 for up
+                temp: 72,
+                hum: 34,
+                motion: true, // boolean
+
+        },
+        email:"dotard@down.us",
+        password:"maga",
+        phone:"5555555566",
+        id:9876123
+}
+//array of user account objects
+var users = [user1, user2];
+
+
+
+
+
 // Display Element
 
 function displayElement(hide, show) {
@@ -7,7 +43,25 @@ function displayElement(hide, show) {
 
 // login button
 document.getElementById("login-btn").addEventListener("click", function() {
-    displayElement("login-page","main-page");
+
+        var email = document.getElementById('email').value;
+
+        if (email == user1.email) {
+                displayElement("login-page","main-page");
+                console.log("USER 1 LOGIN"); //replace with call to display function
+                sessionUser = user1;
+
+        }
+        if (email == user2.email) {
+                displayElement("login-page","main-page");
+                console.log("USER 2 LOGIN");//replace with call to display function
+                sessionUser = user2;
+        }
+        if(email != user1.email && email != user2.email) {
+                alert("Wrong Login Details. Try Again.");
+                console.log("WRONG LOGIN INFO");//replace with print to page of inncorrect password or email
+        }
+
 });
 // forgot-password-button
 document.getElementById("forgot-pwd-btn").addEventListener("click", function() {
